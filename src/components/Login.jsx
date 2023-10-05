ad41e6591a05502265a900e4803c471ac1705ee4
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-10-05 15:03:24 -0400

    login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,55 @@
 /* TODO - add your code to create a functional React component that renders a login form */
 
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    async function loginSubmit() {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
+            })
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            setError(e.message)
+        }
+
+    }
+
     return (
-        <p>Login Form</p>
+        <>
+            <h3>Login</h3>
+
+            {error && <p>Unable to login</p>}
+
+            <form method="POST" onSubmit={loginSubmit} >
+                <label>
+                    Email:{" "}
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                <label>
+                    Password:{" "}
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <button disabled={error} type="submit">Login</button>
+            </form>
+
+
+
+        </>
     )
 
 }
