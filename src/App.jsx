351c5f9467b9d31eb6bb7841dd580bdfa0ee4783
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-10-05 20:10:56 -0400

    good pg

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { useState } from 'react'
 import bookLogo from './assets/books.png'
 import Books from './components/Books'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import SingleBook from './components/SingleBook'
+import Register from './components/Register'
+import Login from './components/Login'
 
 function App() {
   const [token, setToken] = useState(null)
 
   return (
     <>
-    <Routes> 
-      <Route path="/books" element={<Books/>}/>
-      <Route path="/books/:id" element={<SingleBook/>}/>
-  
-    </Routes>
+      <Routes>
+        <Route path="/books" element={<Books />} />
+        <Route path="/books/:id" element={<SingleBook />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+
+      </Routes>
     </>
   )
 }
