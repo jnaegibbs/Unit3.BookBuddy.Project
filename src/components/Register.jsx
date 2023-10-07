6556983ab73ba1fcfa20cbf920aaac9c2679c994
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-10-07 18:13:17 -0400

    change

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,6 @@
 /* TODO - add your code to create a functional React component that renders a registration form */
 import {React, useState } from 'react';
+import {useRegisterMutation} from './API/bookBuddyApi'
 
 export default function Register() {
     const [firstName, setFirstName] = useState("");
@@ -8,28 +9,20 @@ export default function Register() {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
+    const [register] = useRegisterMutation();
+
     async function registerSubmit(event){
-        event.preventDefault();
+        // event.preventDefault();
 
-        try{
-            const response = await fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/register", {
-                method:"POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    firstname: firstName,
-                    lastname: lastName,
-                    email: email,
-                    password: password 
-                })
-            })
-            const result = await response.json();
-            console.log(result);
-        } catch(error){
-            setError(e.message);
-        }
+        // register({firstname: firstName, 
+        //         lastname: lastName,
+        //         email: email, 
+        //         password: password});
+        
+        // {error && <p>unable to perform submission</p>}
+   
 
+        
     }
 
 
