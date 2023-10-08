6ed8648828d3d0893d0c91926e6feafb9b9e4f3d
J’nae Gibbs <jnaegibbs@jnaes-air.lan>
2023-10-07 22:17:03 -0400

    BookStyles

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,63 +1,81 @@
 import { useState, useEffect } from 'react';
 import React from "react";
-import {useNavigate, useParams} from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import TextField from "@mui/material/TextField";
 import { useFetchBooksQuery } from './API/bookBuddyApi';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
 
 
 
 
 const Books = () => {
 
-    
-     const {data,error,isLoading} = useFetchBooksQuery();
-      console.log(data)
+    const { data, error, isLoading } = useFetchBooksQuery();
+    console.log(data)
     const [searchParam, setSearchParam] = useState("");
     const navigate = useNavigate();
-    const bookId =useParams();
+    const bookId = useParams();
     if (isLoading) {
-		return <div>Loading...</div>;
-	}
+        return <div>Loading...</div>;
+    }
 
-  if (error) {
-		return <div>Error: {error.message}</div>;
-	}
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
 
     const filteredBooks = searchParam
-             ? data.books.filter((book)=>
-             book.title.toLowerCase().includes(searchParam)
-             )
-             : data.books;
-            
-    return(
-        <div>
-            
+        ? data.books.filter((book) =>
+            book.title.toLowerCase().includes(searchParam)
+        )
+        : data.books;
+
+    return (
+        <>
             <div>
                 <label>
 
-                    <TextField 
+                    <TextField
                         type="text"
                         placeholder="Search"
                         onChange={(e) => setSearchParam(e.target.value.toLowerCase())}
-                        style={{marginLeft:"30%",width:"40%",padding:"2%"}}
+                        style={{ marginLeft: "30%", width: "40%", padding: "2%" }}
                     />
-    
                 </label>
             </div>
+            <Card sx={{m: 10, border: 2, padding: 5, width: 300, maxWidth: "100%" }}>
             {error ? (
                 <p>{error}</p>
-            ) : (
-                <ul>
-                    {filteredBooks && filteredBooks.map((book) => (
-                        <li key={book.id}>{book.title}
-                             <button onClick={() => navigate(`/books/${book.id}`)}>More Info</button> 
-                        <img src={book.coverimage} />
-                        </li>
-                        ))}
-                </ul>
+                ) : (
+                    <>
+                        {filteredBooks && filteredBooks.map((book) => (
+                    <div>
+                            
+                                <CardMedia
+                                    component="img"
+                                    alt={data.name}
+                                    image={book.coverimage} />
+                                <CardContent
+                                    key={book.id}>{book.title}
+                                </CardContent>
+                                <Button 
+                                variant="contained"
+                                color="primary"
+                                onClick={() => navigate(`/books/${book.id}`)}>More Info</Button>
+                          
+                    </div>
+                       ))}
+                </>
             )
-            }
-        </div>
-    );   }
+        }
+        </Card>
+    </>
+    );
+};
 
 export default Books;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import {useParams} from "react-router-dom";
 import { useFetchBookByIdQuery } from './API/bookBuddyApi';
-
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const SingleBook = () => {
     const bookId = useParams();
@@ -17,12 +22,19 @@ const SingleBook = () => {
     return(
         <>
         {data &&
-        <div>
+        <Card sx={{m: 10, border: 2, padding: 5, width: 400, maxWidth: "100%"}}>
+           <CardMedia
+                component="img"
+                alt={data.name}
+                height="50%"
+                image={data.book.coverimage} 
+            />
+           <CardContent> 
             <h2>{data.book.title}</h2>
             <h4>{data.book.author}</h4>
-            <img src={data.book.coverimage} alt={data.name}/>
             <h4>{data.book.description}</h4>
-        </div>}
+        </CardContent> 
+        </Card>}
         </>
     )
 }
