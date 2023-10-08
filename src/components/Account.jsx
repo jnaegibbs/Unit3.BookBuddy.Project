7f870aae18ee2300ad06b91a61341de5219e87fa
divya040392 <divya.praveen.5215@gmail.com>
2023-10-08 10:26:06 -0500

    condition-added

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,45 +1,36 @@
-import {React} from "react";
-import {useSelector} from "react-redux";
+import { React } from "react";
+import { useSelector } from "react-redux";
 import { useFetchUserQuery } from "./API/bookBuddyApi";
 
 const Account = () => {
+  const token = useSelector((state) => state.token);
 
-    const token = useSelector(state => state.token)
+  console.log("TOKEN: ", token);
 
-    console.log("TOKEN: ", token);
+  const { data, error, isLoading } = useFetchUserQuery(token);
+  console.log(data);
 
-    const {data, error, isLoading} = useFetchUserQuery(token);
+  {
+    isLoading && <p>Loading ...</p>;
+  }
 
-    {isLoading && <p>Loading ...</p>}
+  {
+    error && <p>Not Authorized User</p>;
+  }
 
-    {error && <p>Not Authorized User</p>}
+  return (
+    <>
+      {data && (
+        <div>
+          <h2>user details</h2>
+          <p>{data.firstname} </p>
+          <p>{data.lastname} </p>
+          <p>{data.email} </p>
+          <h2>checked out books here</h2>
+        </div>
+      )}
+    </>
+  );
+};
 
-
-
-     
-
-    
-    return (
-        <>
-
-        
-        <h2>user details</h2>
-       <p>{data.firstname} </p>
-       <p>{data.lastname} </p>
-        <p>{data.email} </p>
-        <h2>checked out books here</h2> 
-        
-        </>
-    )
-}
-
-
-
-export default Account 
-
-
-
-
-
-
-
+export default Account;
