f02c9a897fc0b928e86c22f5761753bd6da1ee40
divya040392 <divya.praveen.5215@gmail.com>
2023-10-07 19:59:15 -0500

    Done-login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,31 +3,23 @@
 /* TODO - add your code to create a functional React component that renders a login form */
 
 import { React, useState } from 'react';
+import { useLoginMutation } from './API/bookBuddyApi';
 
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
+  
+    const [login ] = useLoginMutation();
 
     async function loginSubmit(event) {
         event.preventDefault();
 
-        try {
-            const response = await fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login", {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    email: email,
-                    password: password
-                })
-            })
-            const result = await response.json();
-            console.log(result);
-        } catch (error) {
-            setError(e.message)
-        }
+     const response = await login({
+        email:email,
+        password:password
+     })
+
+     console.log(response);
 
     }
 
