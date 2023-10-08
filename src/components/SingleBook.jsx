c0a32fe9a60b34bc222677d6dc2b772c318e47a0
Mari <dennismaricarl@gmail.com>
2023-10-07 17:42:14 -0700

    SingleBook updated

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,41 +1,32 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import {useParams} from "react-router-dom";
+import { useFetchBookByIdQuery } from './API/bookBuddyApi';
+
+
 const SingleBook = () => {
-    const[singleBook, setSingleBook] = useState(null)
-    const[error, setError] = useState(null)
-    const {id} = useParams();
-    useEffect(()=> {
-        async function fetchSingleBook() {
-            try {
-                const response = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books/${id}`);
-                const result = await response.json();
-                console.log(result);
-                setSingleBook(result)
-            } catch(error) {
-                setError(error.message);
-            }
-        }
-        fetchSingleBook();
-    }, [])
+    const bookId = useParams();
+    const {data, error, isLoading} = useFetchBookByIdQuery(bookId.id)
+    console.log(bookId)
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>Error: {error.message}</div>
+    }
     return(
-
         <>
-
-        {singleBook && 
-        <div> 
-            <h2>{singleBook.book.title}</h2>
-            <h4>{singleBook.book.author}</h4>
-            <img src={singleBook.book.coverimage} alt={singleBook.name}/>
-            <h4>{singleBook.book.description}</h4> 
-        </div>}   
+        {data &&
+        <div>
+            <h2>{data.book.title}</h2>
+            <h4>{data.book.author}</h4>
+            <img src={data.book.coverimage} alt={data.name}/>
+            <h4>{data.book.description}</h4>
+        </div>}
         </>
     )
 }
 export default SingleBook
-
-
-
 
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,7 @@ import store from "../src/components/API/store.js"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-   
       <App />
-    
     </Provider>
   </React.StrictMode>,
 )
